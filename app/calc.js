cf4c03a034819f42b688064fa297612807350df5
maxim <horamax@yandex.ru>
2019-10-28 16:14:34 +0300

    first commit

---
+++ app/calc.js
@@ -0,0 +1,7 @@
+// app/calc.js
+const sum = arr => {
+    return arr.reduce(function(a, b) {
+        return a + b
+    }, 0)
+}
+module.exports.sum = sum
---
+++ app/index.js
@@ -0,0 +1,29 @@
+const calc = require('./calc')
+const express  = require('express');
+const app  = express();
+
+
+//массив данных
+const numbersToAdd = [
+    1,
+    4,
+    10,
+    2
+]
+
+
+
+const result = calc.sum(numbersToAdd) //вызов фунции sum из модуля calc
+console.log(`The result is: ${result}`)
+
+
+//вывод в браузер
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1> ' + result);
+    
+});
+
+
+app.listen(3000, () => {
+    console.log('Example app listenig on port 3000!');
+});
