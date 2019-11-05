9be93a87506c3b09dbcc364c5de039f16426b07e
Horocek <horamax@yanndex.ru>
2019-11-05 21:56:17 +0300

    final work 5 nov

--- app/index.js
+++ app/index.js
@@ -5,17 +5,12 @@ const app  = express();
 
 //вывод в браузер
 app.get('/', (req, res) => {
-<<<<<<< HEAD
     res.send('<h1>Hello World!</h1> ' + result);
 });
 
 app.get('/num=*&num=*', (req, res) => {
     const result = calc.sum(req.path); //вызов фунции sum из модуля calc
     res.send(`<h1>sum = ${result}</h1>`);
-=======
-    res.send(`<h1>Hello World!</h1> ${result}`);
-    
->>>>>>> 1ddff24659d0fdf84b68ab9a841a575abff2bf59
 });
 
 
