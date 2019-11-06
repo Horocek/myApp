54aa731ed0b90224d25e17f428187fb167a00f63
maxim <horamax@yandex.ru>
2019-11-06 17:51:14 +0300

    registration

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const app  = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const users = [
+let users = [
     {
     name : 'Haro',
     pass : '12345678'},
@@ -12,7 +12,7 @@ const users = [
     name : 'Kum',
     pass : 'kum'}
 ]
-
+let accUser = users.length; //кол-во пользователей
 
 app.get('/num=*&num=*', (req, res) => {
     const result = calc.sum(req.path); 
@@ -24,12 +24,21 @@ app.get('/num=*&num=*', (req, res) => {
 app.post('/sum', (req, res) => {
     const result = +req.body.num1 + +req.body.num2;
     res.send(`вычисление суммы -> ${result}`);
-})
+});
+
+//Post /registration регистрация  
+app.post('/registration', (req, res) => {
+    users.push({
+        name : req.body.userName,
+        pass : req.body.userPass,});
+    res.send('Регистрация успешна');
+});
+
 //Post /login авторизация   
 app.post('/login', (req, res) => {
     const result = (users.find(({name, pass}) => req.body.userName === name && req.body.userPass === pass)) ? 'успешная': 'не верная пара'; 
     res.send(`авторизация ${result}`);
-})
+});
 
 app.listen(3000, () => {
     console.log('Example app listenig on port 3000!');
