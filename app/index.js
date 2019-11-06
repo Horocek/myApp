fe9d75dfc86ef4d155f1b8f852bd4e0f1edfed71
maxim <horamax@yandex.ru>
2019-11-06 15:44:21 +0300

    post login and sum

--- app/index.js
+++ app/index.js
@@ -1,18 +1,35 @@
 const calc = require('./calc')
 const express  = require('express');
+const bodyParser = require('body-parser')
 const app  = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
+const users = [
+    {
+    name : 'Haro',
+    pass : '12345678'},
+    {
+    name : 'Kum',
+    pass : 'kum'}
+]
 
-//вывод в браузер
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
 
 app.get('/num=*&num=*', (req, res) => {
-    const result = calc.sum(req.path); //вызов фунции sum из модуля calc
+    const result = calc.sum(req.path); 
     res.send(`<h1>вычисление суммы -> ${result}</h1>`);
 });
 
+
+// Post /sum сложение двух чисел
+app.post('/sum', (req, res) => {
+    const result = +req.body.num1 + +req.body.num2;
+    res.send(`вычисление суммы -> ${result}`);
+})
+//Post /login авторизация   
+app.post('/login', (req, res) => {
+    const result = (users.find(({name, pass}) => req.body.userName === name && req.body.userPass === pass)) ? 'успешная': 'не верная пара'; 
+    res.send(`авторизация ${result}`);
+})
 
 app.listen(3000, () => {
     console.log('Example app listenig on port 3000!');
